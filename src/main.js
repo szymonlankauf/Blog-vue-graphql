b82dcfb85af370089e5453390366f6110e796742
tesla369 <tesla369@gmail.com>
2019-04-02 15:08:43 +0200

    Created user authorization method

--- src/main.js
+++ src/main.js
@@ -3,13 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
-
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
+import { setContext } from 'apollo-link-context'
+
+Vue.config.productionTip = false
+
+const authLink = setContext((_,{ headers }) => {
+
+    // get the authorization token from local storage
+    const token = localStorage.getItem('blog-app-token')
+
+    // return headers to the context, so the httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : null
+        }
+    }
+})
 
 // set url to GraphQL server
 const httpLink = new HttpLink({
@@ -18,7 +32,7 @@ const httpLink = new HttpLink({
 
 // create Apollo client
 const apolloClient = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
 
@@ -36,3 +50,6 @@ new Vue({
     components: { App },
     template: '<App/>'
 })
+
+
+
